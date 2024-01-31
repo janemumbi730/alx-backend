58e9989353877dc7eb75d21bb76239e9d6afe52b
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2024-01-31 15:23:55 +0300

    lifo cache

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Module 2-lifo_cache
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    Class inherits from BaseCaching
+    """
+    def __init__(self):
+        """Initialization"""
+        super().__init__()
+        self.__keys = []
+
+    def put(self, key, item):
+        """Adds an item to the dict and pops using LIFO"""
+        if len(self.cache_data) == self.MAX_ITEMS and key not in self.__keys:
+            to_del = self.__keys.pop()
+            del self.cache_data[to_del]
+            print("DISCARD: {}".format(to_del))
+
+        if key and item:
+            self.__keys.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieves an item by key"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
